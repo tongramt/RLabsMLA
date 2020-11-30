f8513eaa346fbb02a2497d0dcc19d59c758ac4d0
tongramt <grantth@tcd.ie>
2020-11-30 10:19:32 +0000

    MLA Lab 1

--- Lab1.R
+++ Lab1.R
@@ -1,10 +1,23 @@
-# Title     : TODO
-# Objective : TODO
+# Title     : Lab 1
+# Objective : Write and annotate code for lab 1
 # Created by: ThomasGrant
 # Created on: 26/10/2020
+# Using the help function in r
+?exp
+# using functions such as logs and storing variables
+h <- log(10, base = 10)
+i <- log(10, base = 2)
 y <- exp(2.5)
 y
+# Setting up matrices in r
 a <- matrix(c(1,2,3,4),nrow = 2, ncol = 2, byrow = TRUE,)
 a
 b <- matrix(c(1,2,2,3,4,5), nrow = 3, ncol = 2, byrow = TRUE)
 b
+# Installing packages in r
+install.packages("cluster")
+install.packages("mclust")
+# Downloading datasets in r
+music <- read.csv("https://www.scss.tcd.ie/~arwhite/Teaching/STU33011/music.csv")
+# displaying data from datasets
+head(music, 10)
